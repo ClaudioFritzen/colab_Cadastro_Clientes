74f8a7ac247c21bc0b3bb5e7a15c4256644da80d
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-04 00:21:55 +0100

    criando a pag teste de login

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -4,4 +4,6 @@ from django. http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Você esta na pagina inicial")
-    
+
+def login(request):
+    return HttpResponse("Você esta em login")
