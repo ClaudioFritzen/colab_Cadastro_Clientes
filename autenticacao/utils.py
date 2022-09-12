f2222483f4b06fd6d89e743a63d415c4c913f1dd
ClaudioFritzen <sdfritzen96@gmail.com>
2022-09-12 23:23:04 +0100

    criando a função de enviar email

--- autenticacao/utils.py
+++ autenticacao/utils.py
@@ -1,6 +1,13 @@
+from cgitb import html
 import re
 from django.contrib import messages
 from django.contrib.messages import constants
+
+# importações para email
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 def password_is_valid (request, password, confirm_password):
     if len(password) < 6:
@@ -25,3 +32,15 @@ def password_is_valid (request, password, confirm_password):
         return False
 
     return True
+
+# funcão para enviar o token de aticação do email
+def send_email_html(path_template: str, assunto: str, para: list, **kwargs ) -> dict:
+    
+    html_content = render_to_string(path_template, kwargs)
+    text_content = strip_tags(html_content)
+
+    email = EmailMultiAlternatives(assunto, text_content, settings.EMAIL_HOST_USER, para)
+
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    return {'status': 1}
